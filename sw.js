9c23c5918d1bd11ffcf0e2cb2899c4a5995543ca
idikale163-source <idikale163@gmail.com>
2026-08-04 20:57:59 +0800

    chore: bump cache v1785848273

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "resource-hub-v1785847854";
+const CACHE_NAME = "resource-hub-v1785848273";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => {
     e.waitUntil(
