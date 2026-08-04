6d8a15cd328e5511c6671689c553dab263dd6361
idikale163-source <idikale163@gmail.com>
2026-08-04 20:34:57 +0800

    Revert 1:1 to bbc197f (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "resource-hub-v1785846179";
+const CACHE_NAME = "resource-hub-v1785846876";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => {
     e.waitUntil(
