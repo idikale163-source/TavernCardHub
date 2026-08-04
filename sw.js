270e298b845afe29ec8e91b9e99d9e57cb99413f
idikale163-source <idikale163@gmail.com>
2026-08-04 18:28:58 +0800

    chore(sw): force bump cache version for display none important fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'resource-hub-v1785838436';
+const CACHE_NAME = 'resource-hub-v1785839337';
 self.addEventListener('install', (e) => { self.skipWaiting(); });
 self.addEventListener('activate', (e) => {
     e.waitUntil(
