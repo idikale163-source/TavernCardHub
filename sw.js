55d9b653cd75a5d7825a57f1122bee0277627510
idikale163-source <idikale163@gmail.com>
2026-08-04 18:45:04 +0800

    chore: force clear sw cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'resource-hub-v1785839337';
+const CACHE_NAME = 'resource-hub-v1785840283';
 self.addEventListener('install', (e) => { self.skipWaiting(); });
 self.addEventListener('activate', (e) => {
     e.waitUntil(
