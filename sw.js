1f6f6ce69faef6bf35c64dcff397e24a6aa826f7
idikale163-source <idikale163@gmail.com>
2026-08-04 20:15:37 +0800

    chore: force clear cache v1785845735

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'resource-hub-v1785840283';
-self.addEventListener('install', (e) => { self.skipWaiting(); });
-self.addEventListener('activate', (e) => {
+const CACHE_NAME = "resource-hub-v1785845735";
+self.addEventListener("install", (e) => { self.skipWaiting(); });
+self.addEventListener("activate", (e) => {
     e.waitUntil(
         caches.keys().then((keys) => {
             return Promise.all(keys.map((k) => caches.delete(k)));
         }).then(() => self.clients.claim())
     );
 });
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
     e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
